b27ad4253acf570f5a9be7e7606eaf9bed138d82
akukeano <keanoraeehan@gmail.com>
2025-11-26 21:21:41 +0700

    Add files via upload

--- Pengurangan matriks.cpp
+++ Pengurangan matriks.cpp
@@ -1,47 +1,47 @@
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+	int r, c, a[100][100], b[100][100], sub[100][100], i, j;
 
     cout << "Masukkan jumlah baris (1 - 100): ";
     cin >> r;
 
     cout << "Masukkan jumlah kolom (1 - 100): ";
     cin >> c;
-
-    cout << endl << "Masukkan matriks pertama:" << endl;
+    
+    cout << endl << "Masukan matriks pertama:" << endl;
     for (i = 0; i < r; ++i) {
         for (j = 0; j < c; ++j) {
             cout << "Matriks a[" << i + 1 << "][" << j + 1 << "]: ";
             cin >> a[i][j];
-        }
-    }
-
-    cout << endl << "Masukkan matriks kedua:" << endl;
+   		 }
+	}
+	
+	cout << endl << "Masukkan matriks kedua:" << endl;
     for (i = 0; i < r; ++i) {
         for (j = 0; j < c; ++j) {
             cout << "Matriks b[" << i + 1 << "][" << j + 1 << "]: ";
             cin >> b[i][j];
         }
     }
-
-    // Proses penjumlahan1
+    
+    // Proses pengurangan
     for (i = 0; i < r; ++i) {
         for (j = 0; j < c; ++j) {
-            sum[i][j] = a[i][j] + b[i][j];
+            sub[i][j] = a[i][j] - b[i][j];
         }
     }
 
-    cout << endl << "Hasil penjumlahan matriks:" << endl;
+	cout << endl << "Hasil pengurangan matriks:" << endl;
     for (i = 0; i < r; ++i) {
         for (j = 0; j < c; ++j) {
-            cout << sum[i][j] << " ";
+            cout << sub[i][j] << " ";
         }
         cout << endl;
     }
 
-    return 0;
 }
-
+	
+    
+    
